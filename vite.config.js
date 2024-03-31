daec131dba77e24909028a4e3a14f4ccda661d0b
Pablo Sarzosa <pavosarzosa20@gmail.com>
2024-03-30 22:26:29 -0500

    Change in vite.config.js file to deply

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig(
     
   plugins: [react()],
   build: {
-    outDir: 'build', // Cambia 'dist' a 'build' o a cualquier otro nombre de carpeta que prefieras
+    outDir: 'dist', // Cambia 'dist' a 'build' o a cualquier otro nombre de carpeta que prefieras
   },
-  base: './', // Cambia '/react-vite/' por el nombre de la carpeta que prefieras
+  //base: './', // Cambia '/react-vite/' por el nombre de la carpeta que prefieras
 
   resolve: {
     alias: {
